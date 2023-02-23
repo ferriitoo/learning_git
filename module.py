3e6396c94a22cc5132cc9e1004d9fc3b29d73a83
ferriitoo <ferrojulen@gmail.com>
2023-02-23 16:41:17 -0600

    Adding a library module

---
+++ module.py
@@ -0,0 +1,11 @@
+
+
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    
+    def area(self):
+        area = self.width * self.height 
+        return area
+        self.area = area
